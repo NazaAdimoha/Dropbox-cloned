f61f9dc13196e3ae0ff28840ec7d162365380add
nazaadimoha <nazaadimoha98@gmail.com>
2023-12-13 20:02:23 +0100

    create a skeleton loaded table wrapper component

---
+++ components/table/TableWrapper.tsx
@@ -0,0 +1,10 @@
+
+const TableWrapper = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TableWrapper
---
+++ typings.ts
@@ -0,0 +1,10 @@
+
+export type FileType = {
+    id: string;
+    fileName: string;
+    fullName: string;
+    timestamp: Date;
+    downloadUrl: string;
+    type: string;
+    size: number;
+};
